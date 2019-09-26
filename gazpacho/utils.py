8088d790c5a9277aa29334cfc3c27f040bfd230b
Max Humber <max.humber@gmail.com>
2019-09-26 15:50:03 -0400

    utils docs

--- gazpacho/utils.py
+++ gazpacho/utils.py
@@ -1,23 +1,63 @@
-def match(query_attrs, attrs):
-    '''Match a query dictionary to an attrs dictionary'''
-    if not query_attrs:
+def match(a, b, strict=False):
+    '''A utility function to match two dictionaries
+
+    a: dict
+        The query dictionary
+    b: dict
+        The dictionary to match
+    strict: bool
+        Require exact matching
+
+    Examples:
+
+    >>> a = {'foo': 'bar'}
+    >>> b = {'foo': 'bar baz'}
+    >>> match(a, b)
+    True
+
+    >>> a = {}
+    >>> b = {'foo': 'bar'}
+    >>> match(a, b)
+    True
+    '''
+    if not a:
         return True
-    if not query_attrs and not attrs:
+    if not a and not b:
         return True
-    if query_attrs and not attrs:
+    if a and not b:
         return False
-    bools = []
-    for k, v in query_attrs.items():
-        if not attrs.get(k):
-            bools.append(False)
-        elif v in attrs.get(k):
-            bools.append(True)
+    for k, v in a.items():
+        if not b.get(k):
+            return False
+        if strict:
+            if v == b.get(k):
+                continue
+            else:
+                return False
+        if v in b.get(k):
+            continue
         else:
-            bools.append(False)
-    return all(bools)
+            return False
+    return True
 
 def html_starttag_and_attrs(tag, attrs, startendtag=False):
-    '''Reconstruct starttag and convert attrs to a dictionary'''
+    '''A utility functon to reconstruct starttag and attrs
+
+    tag: str
+        An HTML tag
+    attrs: list
+        A list of tuples for HTML tag attributes
+    startendtag: bool
+        A flag to handle startend tags
+
+    returns:
+        html (str), attrs (dict)
+
+    Example:
+
+    >>> html_starttag_and_attrs('a', [('href', 'localhost:8000')])
+    ('<a href="localhost:8000">', {'href': 'localhost:8000'})
+    '''
     if attrs:
         attrs = dict(attrs)
         af = [f'{k}="{v}"' for k, v in attrs.items()]
