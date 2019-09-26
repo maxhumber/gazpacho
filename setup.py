d5f4c2d7460fa983be0ff54f832ef5f03018c24d
Max Humber <max.humber@gmail.com>
2019-09-26 17:05:48 -0500

    Deployed  with MkDocs version: 1.0.4

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf8') as f:
+    long_description = f.read()
+
+setup(
+    name='gazpacho',
+    version='0.6.5',
+    description='Web scraping with pure python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=[
+        'web', 'web scraping', 'scraping', 'beautifulsoup'
+    ],
+    url='https://github.com/maxhumber/gazpacho',
+    author='Max Humber',
+    author_email='max.humber@gmail.com',
+    license='MIT',
+    package=['gazpacho'],
+    python_requires='>=3.6',
+    setup_requires=['setuptools>=38.6.0']
+)
