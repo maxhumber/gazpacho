aa304aece2f187198e5651cd20517c51413eb925
Max Humber <max.humber@gmail.com>
2019-10-07 15:18:50 -0500

    Deployed  with MkDocs version: 1.0.4

--- sandbox.py
+++ sandbox.py
@@ -1,34 +1,52 @@
+try:
+    from .utils import match, html_starttag_and_attrs
+except ModuleNotFoundError:
+    from gazpacho.utils import match, html_starttag_and_attrs
+
 from gazpacho import get
 
-html = get('https://maxhumber.com/')
-print(html)
+url = 'https://en.wikipedia.org/wiki/Gazpacho'
+html = get(url)
+print(html[:50])
 
-html = '''<!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89691472-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-89691472-1');
-    </script>
-    <!-- Required meta tags -->
-    <title>Max Humber</title>'''
+# <!DOCTYPE html>
+# <html class="client-nojs" lang="en
 
-import re
+url = 'https://httpbin.org/anything'
+get(url, params={'foo': 'bar', 'bar': 'baz'}, headers={'User-Agent': 'gazpacho'})
 
-x = re.sub('\<script(.*?)\<\/script\>', '', html)
-print(x)
+from gazpacho import Soup
+
+soup = Soup(html)
+str(soup)[:50]
+
+# '<!DOCTYPE html>\n<html class="client-nojs" lang="en'
+
+# Original HTML: <span class="mw-headline" id="Ingredients_and_preparation">Ingredients and preparation</span>
+
+results = soup.find('span', {'class': 'mw-headline'})
+
+len(results)
+
+soup.find('span', {'class': 'mw-headline'}, mode='first')
+
+from gazpacho import get, Soup
+import pandas as pd
+
+url = 'https://www.capfriendly.com/browse/active/2020/salary?p=1'
+response = get(url)
+soup = Soup(response)
+df = pd.read_html(str(soup.find('table')))[0]
+print(df[['PLAYER', 'TEAM', 'SALARY', 'AGE']].head(3))
+
+from gazpacho import Soup
+
+%%timeit
+soup = Soup(html)
+soup.find('span', {'class': 'mw-headline'})
+# 15 ms ± 325 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
 
 
-var x = "abc.cde:abc";
-
-re.sub('\<script[^\<\/script\>]*\<\/script\>', '', html)
- "abc abc"
-
-import xml.etree.ElementTree as ET
-root = ET.fromstring(html)
 
 
-from xml.dom import minidom
-x = minidom.parseString(html, )
-x.toprettyxml()
+#
