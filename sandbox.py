08f387a5fdf48f14314699f84a93273926570875
Max Humber <max.humber@gmail.com>
2019-10-12 22:53:49 -0500

    Deployed  with MkDocs version: 1.0.4

--- sandbox.py
+++ sandbox.py
@@ -1,52 +1,19 @@
-try:
-    from .utils import match, html_starttag_and_attrs
-except ModuleNotFoundError:
-    from gazpacho.utils import match, html_starttag_and_attrs
-
-from gazpacho import get
-
-url = 'https://en.wikipedia.org/wiki/Gazpacho'
-html = get(url)
-print(html[:50])
-
-# <!DOCTYPE html>
-# <html class="client-nojs" lang="en
-
-url = 'https://httpbin.org/anything'
-get(url, params={'foo': 'bar', 'bar': 'baz'}, headers={'User-Agent': 'gazpacho'})
-
 from gazpacho import Soup
 
+html = """
+<div class="foo-list">
+  <a class="foo" href="/foo/1">
+    <div class="foo-image-container">
+      <img src="image.jpg">
+    </div>
+  </a>
+  <a class="foo" href="/foo/2">
+    <div class="foo-image-container">
+      <img src="image.jpg">
+    </div>
+  </a>
+</div>
+"""
+
 soup = Soup(html)
-str(soup)[:50]
-
-# '<!DOCTYPE html>\n<html class="client-nojs" lang="en'
-
-# Original HTML: <span class="mw-headline" id="Ingredients_and_preparation">Ingredients and preparation</span>
-
-results = soup.find('span', {'class': 'mw-headline'})
-
-len(results)
-
-soup.find('span', {'class': 'mw-headline'}, mode='first')
-
-from gazpacho import get, Soup
-import pandas as pd
-
-url = 'https://www.capfriendly.com/browse/active/2020/salary?p=1'
-response = get(url)
-soup = Soup(response)
-df = pd.read_html(str(soup.find('table')))[0]
-print(df[['PLAYER', 'TEAM', 'SALARY', 'AGE']].head(3))
-
-from gazpacho import Soup
-
-%%timeit
-soup = Soup(html)
-soup.find('span', {'class': 'mw-headline'})
-# 15 ms ± 325 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
-
-
-
-
-#
+soup.find("a", {"class": "foo"})
