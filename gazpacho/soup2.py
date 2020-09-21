0c941ed89b8fe9bb9ccca9853e0ab6de111264e8
Max Humber <max.humber@gmail.com>
2020-09-21 18:02:23 -0400

    announcer on 2

--- gazpacho/soup2.py
+++ gazpacho/soup2.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from html.parser import HTMLParser
 
 from gazpacho.utils import match, html_starttag_and_attrs
@@ -20,57 +21,36 @@ class Soup2(HTMLParser):
 
     def handle_starts(self, tag, attrs):
         html, attrs = html_starttag_and_attrs(tag, attrs)
-        matching = match(self.attrs, attrs)
-
-        if tag == self.tag and matching and not self.counter[tag]:
-            self.groups.append(Soup2())
-            self.groups[-1].html += html
-            self.groups[-1].tag = tag
-            self.groups[-1].attrs = attrs
-            self.counter[tag] += 1
-        elif self.counter[tag]:
-
-        self.groups[-1].html += html
+        matching = match(attrs, self.attrs)
         self.counter[tag] += 1
+        print(f'{tag} (start) +1 = {self.counter[tag]}')
+
 
     def handle_starttag(self, tag, attrs):
         self.handle_starts(tag, attrs)
         if self.is_void_tag(tag):
             self.counter[tag] -= 1
+            print(f'{tag} (void)  -1 = {self.counter[tag]}')
 
     def handle_startendtag(self, tag, attrs):
         self.handle_starts(tag, attrs)
         self.counter[tag] -= 1
+        print(f'{tag} (s/end) -1 = {self.counter[tag]}')
 
     def handle_data(self, data):
-        if self.counter[self.tag]:
-            try:
-                if self.groups[-1].text is None:
-                    self.groups[-1].text = data.strip()
-                self.groups[-1].html += data
-            except IndexError:
-                pass
+        pass
 
     def handle_endtag(self, tag):
-        if self.counter[tag]:
-            self.groups[-1].html += f"</{tag}>"
         self.counter[tag] -= 1
+        print(f'{tag} (end)   -1 = {self.counter[tag]}')
 
     def find(self, tag, attrs=None):
         self.tag = tag
         self.attrs = attrs
-        self.count = 0
+        self.counter = Counter()
         self.groups = []
         self.feed(self.html)
         return self.groups
-
-# with open("sand.html", "r") as f:
-#     html = f.read()
-#
-# # one
-# parser = Soup2()
-# parser.feed(html)
-# parser.counter
 
 # two
 html = """
@@ -87,18 +67,4 @@ html = """
 
 soup = Soup2(html)
 soup.find('div')
-
-soup.find('div', {'id': 'hi'})
-
-soup.find('img')
-
-
-sum(soup.counter.values())
-
-soup.groups
-
-
-
-
-
-print(html)
+soup.counter
