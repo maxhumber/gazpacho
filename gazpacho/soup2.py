3757dbd9074fccab44ce335bb01f813d6d7176de
Max Humber <max.humber@gmail.com>
2020-09-22 10:13:06 -0400

    I THINK I GOT IT?

--- gazpacho/soup2.py
+++ gazpacho/soup2.py
@@ -3,11 +3,17 @@ from html.parser import HTMLParser
 
 from gazpacho.utils import match, html_starttag_and_attrs
 
+debug = False
+
 class Soup2(HTMLParser):
 
     @staticmethod
     def void(tag):
         return tag in ["img", "link"]
+
+    @property
+    def recording(self):
+        return sum(self.counter.values()) > 0
 
     def __repr__(self):
         return self.html
@@ -22,26 +28,55 @@ class Soup2(HTMLParser):
     def handle_start(self, tag, attrs):
         html, attrs = html_starttag_and_attrs(tag, attrs)
         matching = match(self.attrs, attrs)
-        self.counter[tag] += 1
-        print(f'{tag} (start) +1 = {self.counter[tag]}')
+
+        # if match and not already recording
+        if (tag == self.tag) and (matching) and (not self.recording):
+            self.groups.append(Soup2())
+            self.groups[-1].tag = tag
+            self.groups[-1].attrs = attrs
+            self.groups[-1].html += html
+            self.counter[tag] += 1
+            # print(sum(self.counter.values()))
+            return
+
+        # if already recording
+        if self.recording:
+            self.groups[-1].html += html
+            self.counter[tag] += 1
+
+        if debug:
+            print(f'{tag} (start) +1 = {self.counter[tag]}')
 
     def handle_starttag(self, tag, attrs):
         self.handle_start(tag, attrs)
-        if self.void(tag):
-            self.counter[tag] -= 1
+        if self.recording:
+            if self.void(tag):
+                self.counter[tag] -= 1
+
+        if debug:
             print(f'{tag} (void)  -1 = {self.counter[tag]}')
 
     def handle_startendtag(self, tag, attrs):
         self.handle_start(tag, attrs)
-        self.counter[tag] -= 1
-        print(f'{tag} (s/end) -1 = {self.counter[tag]}')
+        if self.recording:
+            self.counter[tag] -= 1
+
+        if debug:
+            print(f'{tag} (s/end) -1 = {self.counter[tag]}')
 
     def handle_data(self, data):
-        pass
+        if self.recording:
+            if self.groups[-1].text is None:
+                self.groups[-1].text = data.strip()
+            self.groups[-1].html += data
 
     def handle_endtag(self, tag):
-        self.counter[tag] -= 1
-        print(f'{tag} (end)   -1 = {self.counter[tag]}')
+        if self.recording:
+            self.groups[-1].html += f"</{tag}>"
+            self.counter[tag] -= 1
+
+        if debug:
+            print(f'{tag} (end)   -1 = {self.counter[tag]}')
 
     def find(self, tag, attrs=None):
         self.tag = tag
@@ -61,9 +96,15 @@ html = """
     <img src="test2.png">
     <img src="test3.png" />
     <div id="hi">Bye</div>
+    <div id="hi2">Gotcha</div>
 </div>
 """
 
 soup = Soup2(html)
-soup.find('img')
-soup.counter
+results = soup.find('div', {'id': 'hi2'})
+
+soup.find('img')[2]
+
+results
+
+#
