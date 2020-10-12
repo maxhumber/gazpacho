c9b7e35ccd88ff9bdd056633a2ab9387077b79f8
am401 <andras.marton@wpengine.com>
2020-10-11 21:19:11 -0500

    Initial commit of randomizing UAs

--- gazpacho/get.py
+++ gazpacho/get.py
@@ -1,4 +1,5 @@
 import json
+import random
 from typing import Any, Dict, Optional, Union
 from urllib.error import HTTPError as UrllibHTTPError
 from urllib.parse import urlencode
@@ -6,9 +7,19 @@ from urllib.request import build_opener
 
 from .utils import HTTPError, sanitize
 
-USER_AGENT = (
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:80.0) Gecko/20100101 Firefox/80.0"
-)
+USER_AGENT = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:80.0) Gecko/20100101 Firefox/80.0",
+    "AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36",
+    "Mozilla/5.0 (Apple-iPhone7C2/1202.466; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543 Safari/419.3"
+]
+
+
+def random_ua(string):
+    list_length = len(string)
+    agent = random.randrange(list_length)
+    random_agent = string[agent]
+    return random_agent
 
 
 def get(
@@ -32,7 +43,7 @@ def get(
     """
     url = sanitize(url)
     opener = build_opener()
-    opener.addheaders = list({**{"User-Agent": USER_AGENT}, **headers}.items())
+    opener.addheaders = list({**{"User-Agent": random_ua(USER_AGENT)}, **headers}.items())
     if params:
         url += "?" + urlencode(params)
     try:
